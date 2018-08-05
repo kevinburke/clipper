c96fc810f418d33281c3316668b4a015fe8a079f
Kevin Burke <kev@inburke.com>
2018-08-05 12:35:13 -0700

    0.4

--- cmd/clipper-server/main.go
+++ cmd/clipper-server/main.go
@@ -70,7 +70,7 @@ func commonMain() (*FileConfig, http.Handler) {
 var DefaultPort = 8540
 
 // The server's Version.
-const Version = "0.3"
+const Version = "0.4"
 
 // FileConfig represents the data in a config file.
 type FileConfig struct {
